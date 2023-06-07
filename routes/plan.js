ab58053d559877eae90f044d5f7dd63f3735161b
David Perelman <dperelman3@gmail.com>
2023-06-07 22:49:00 +0300

    setup plan input html page

--- routes/plan.js
+++ routes/plan.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.render('plan');
+});
+
+router.post('/', (req, res) => {
+  const plan_num = req.body.plan_number;
+  res.send(`plan: ${plan_num}`);
+  console.log(`plan: ${plan_num}`);
+});
+
 router.get('/:plan_number', (req, res) => {
   const plan_num = req.params.plan_number;
   res.send(`plan: ${plan_num}`);
