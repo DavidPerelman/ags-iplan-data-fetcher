7be3f66d1fe42f725eabbef0149ba030fd5a2c60
David Perelman <dperelman3@gmail.com>
2023-06-07 22:10:25 +0300

    setup views

--- server.js
+++ server.js
@@ -5,4 +5,6 @@ app.get('/', (req, res) => {
   //   res.download('server.js');
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Application started and Listening on port 3000');
+});
