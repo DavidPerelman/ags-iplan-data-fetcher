0730ad5df48fa03845348b5c67770adf3ca43067
David Perelman <dperelman3@gmail.com>
2023-06-27 17:18:54 +0300

    remove not in use libraries

--- server.js
+++ server.js
@@ -7,33 +7,13 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Front
 app.set('view engine', 'ejs');
 
+// Routes
 const planRouter = require('./routes/plan');
 const polygonRouter = require('./routes/polygon');
 const coordinatesRouter = require('./routes/coordinates');
-
-app.post('/upload', function (req, res) {
-  let sampleFile;
-  let uploadPath;
-
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('No files were uploaded.');
-  }
-
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  sampleFile = req.files.sampleFile;
-  uploadPath = __dirname + '/somewhere/on/your/server/' + sampleFile.name;
-
-  console.log(sampleFile);
-  return;
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv(uploadPath, function (err) {
-    if (err) return res.status(500).send(err);
-
-    res.send('File uploaded!');
-  });
-});
 
 app.use('/plan', planRouter);
 app.use('/polygon', polygonRouter);
